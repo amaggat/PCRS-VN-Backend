0a5a619ce320a57c44173f9bb18c4a9b982a3c8d
Aleister <detuongtuongthat@gmail.com>
2020-12-27 11:32:15 +0700

    database update

---
+++ src/main/resources/database/Sql init file/pcprofile.sql
@@ -0,0 +1,9 @@
+INSERT IGNORE `pc_parts_picker`.`pcprofile` VALUES ('first-build', '0', 'First PC Build');
+INSERT IGNORE `pc_parts_picker`.`pcprofile_cpu` VALUES ('first-build', 'amd-ryzen-5-3600');
+INSERT IGNORE `pc_parts_picker`.`pcprofile_mainboard` VALUES ('first-build', 'asrock-b450m-steel-legend');
+INSERT IGNORE `pc_parts_picker`.`pcprofile_ram` VALUES ('first-build', 'corsair-dominator-platinum-2x8G-3200');
+INSERT IGNORE `pc_parts_picker`.`pcprofile_gpu` VALUES ('first-build', 'msi-geforce-rtx-3060-ti-gaming-x-trio');
+INSERT IGNORE `pc_parts_picker`.`pcprofile_hdd` VALUES ('first-build', 'seagate-firecuda-1024-2.5sata');
+INSERT IGNORE `pc_parts_picker`.`pcprofile_ssd` VALUES ('first-build', 'kingston-a2000-500-m2-nvme');
+INSERT IGNORE `pc_parts_picker`.`pcprofile_psu` VALUES ('first-build', 'cm-mwe-gold-650-gold');
+
