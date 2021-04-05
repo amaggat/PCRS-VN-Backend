bd44e76cf43eb03439220c2f562833876c651df0
Aleister <detuongtuongthat@gmail.com>
2021-04-05 14:04:24 +0700

    Refactoring

---
+++ src/main/java/com/PcPartPicker/BackEnd/Feedbacks/FeedbacksRepository.java
@@ -0,0 +1,17 @@
+package com.pcpartpicker.backend.feedbacks;
+
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface FeedbacksRepository extends Repository<Feedbacks, Integer>, JpaSpecificationExecutor<Feedbacks> {
+
+    void save(Feedbacks feedbacks);
+
+    @Query("SELECT DISTINCT feedback from Feedbacks feedback WHERE feedback.post.id= :postid")
+    List<Feedbacks> findByPostID(@Param("postid") Integer postid);
+}
---
+++ src/main/java/com/PcPartPicker/BackEnd/pc/component/psu/PowerSupplyUnit.java
@@ -0,0 +1,65 @@
+package com.pcpartpicker.backend.pc.component.psu;
+
+
+import com.pcpartpicker.backend.pcprofile.PcProfile;
+import com.pcpartpicker.backend.pc.electronicComponents;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+
+@Entity
+@Table(name ="psu")
+public class PowerSupplyUnit extends electronicComponents {
+
+    @Column(name = "power")
+    @NotEmpty
+    private Integer power;
+
+    @Column(name = "standard_80")
+    @NotEmpty
+    private String standard_80 = new String();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "psu", fetch = FetchType.EAGER)
+    private List<psuPriceList> PriceList;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "psu")
+    private List<PcProfile> pcProfile;
+
+    public List<psuPriceList> getPriceList() {
+        return PriceList;
+    }
+
+    public void setPriceList(List<psuPriceList> psuPriceList) {
+        this.PriceList = psuPriceList;
+    }
+
+    //    @Column(name = "size")
+//    @NotEmpty
+//    private String size = new String();
+
+//    public String getSize() {
+//        return size;
+//    }
+
+    public String getStandard_80() {
+        return standard_80;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+//    public void setSize(String size) {
+//        this.size = size;
+//    }
+
+    public void setStandard_80(String standard_80) {
+        this.standard_80 = standard_80;
+    }
+
+    public void setPower(Integer power) {
+        power = power;
+    }
+}
