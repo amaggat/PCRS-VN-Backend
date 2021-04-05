36bd13c6ea89ee58ec21248df5aa67f4a42cd920
Aleister <detuongtuongthat@gmail.com>
2021-04-05 13:56:21 +0700

    Refactoring

--- src/main/java/com/PcPartPicker/BackEnd/BackEndApplication.java
+++ src/main/java/com/PcPartPicker/BackEnd/BackEndApplication.java
@@ -1,4 +1,4 @@
-package com.PcPartPicker.BackEnd;
+package com.pcpartpicker.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
